38d517aae277ac56e7dc16fcc98f60c2111260d7
ahesh <ahesh.alagu@zohocorp.com>
2024-01-30 19:19:36 +0530

    added class BuildMaxHeap

---
+++ source/BuildMaxHeap.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class BuildMaxHeap {
+    
+    public static void swap(int[] arr, int i, int j) {
+        if(i == j) return;
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        
+    }
+
+    public static void maxHeapify(int[] arr, int i) {
+        int temp = i ;
+        int length = arr.length;
+
+        while(2 * temp + 1  < length) {
+            int left = 2 * temp + 1;
+            int right = 2 * (temp + 1);
+            int target = temp;
+
+            if(left < length && arr[left] > arr[temp]) {
+                target = left;    
+            }
+
+            if(right < length && arr[right] > arr[target]) {
+                target = right;
+            }
+
+            if(target == temp) {
+                break;
+            } 
+
+            swap(arr, temp, target);
+            temp = target;
+        }
+    }
+
+    public static void buildMaxHeap(int[] arr) {
+        int length = arr.length;
+        int idx = length / 2 + 1;
+
+        for(int i = idx; i >= 0; i--) {
+            maxHeapify(arr, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 10, 14, 16, 9, 3, 7, 8, 1, 2};
+        buildMaxHeap(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
