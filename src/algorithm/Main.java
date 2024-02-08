58d0b40058695ea9f2d9f1054a8689b1790b09ed
ahesh <aheshalagu@gmail.com>
2024-02-09 00:01:00 +0530

    added class MinHeap

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,19 +1,15 @@
 package algorithm;
 
-import myds.heap.MaxHeap;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import myds.heap.MinHeap;
 
 public class Main {
 	public static void main(String[] args) {
 		Integer[] arr =  {16, 10, 4, 7, 9, 14, 2, 8, 3, 1};
-		MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(arr);
+		MinHeap<Integer> minHeap = new MinHeap<Integer>(arr);
 		
 		try {
-			maxHeap.heapSort();
-			System.out.print(maxHeap.getHeap());
+			minHeap.heapSort();
+			System.out.print(minHeap.getHeap());
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.print("Error");
---
+++ src/myds/heap/MinHeap.java
@@ -0,0 +1,77 @@
+package myds.heap;
+
+import java.util.List;
+
+public class MinHeap<T extends Comparable<T>> extends Heap<T>{
+	
+	public MinHeap() {
+		super();
+	}
+	
+	public MinHeap(T[] arr) {
+		super(arr);
+	}
+	
+	private void swap(List<T>list, int i, int j) {
+		T temp = list.get(i);
+		list.set(i, list.get(j));
+		list.set(j, temp);
+	}
+	
+private void minHeapify(int i, List<T> arr) {
+		
+		int length = super.getSize();
+		
+		if(i > 2 * length + 1) return;
+		
+		int left = 2 * i + 1;
+		int right  = 2 * (i + 1);
+		int target = i;
+		
+		if(
+			left < length && 
+			arr.get(left).compareTo(arr.get(target)) < 0
+		) {
+			target = left;
+			
+		}
+		
+		if(
+			right < length && 
+			arr.get(right).compareTo(arr.get(target)) < 0
+		) {
+			target = right;
+		}
+		
+		if(target != i) {
+			swap(arr, i, target);
+			minHeapify(target, arr);
+		}
+	}
+	
+	
+	public void buildMaxHeap(List<T> arr) {
+		int length = super.getSize();
+		for(int i = length / 2 - 1; i >= 0; i--) {
+			minHeapify(i, arr);
+		}
+	}
+	
+	public void heapSort() throws Exception{
+		List<T> arr = super.getHeap();
+		
+		buildMaxHeap(arr);
+		
+		while(super.getSize() != 0) {
+			int size = super.getSize();
+			swap(arr, size - 1, 0);
+			super.setSize(size - 1);
+			minHeapify(0, arr);
+		}
+		
+	}
+	
+	public List<T> getHeap() {
+		return super.getHeap();
+	}
+}
