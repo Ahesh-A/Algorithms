d7edf65371813aef2156e47ad1a2b2518cf17e8d
ahesh <aheshalagu@gmail.com>
2024-02-09 20:28:20 +0530

    testing with annotations

---
+++ src/algorithm/Cat.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+@VeryImportant
+public class Cat {
+	@ImportantMethod
+	public void meow() {
+		System.out.println("meow...");
+	}
+	
+	@ImportantMethod
+	public void eat() {
+		System.out.println("munching...");
+	}
+}
--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,18 +1,39 @@
 package algorithm;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import myds.heap.MinHeap;
 
 public class Main {
-	public static void main(String[] args) {
-		Integer[] arr =  {16, 10, 4, 7, 9, 14, 2, 8, 3, 1};
-		MinHeap<Integer> minHeap = new MinHeap<Integer>(arr);
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+//		Integer[] arr =  {16, 10, 4, 7, 9, 14, 2, 8, 3, 1};
+//		MinHeap<Integer> minHeap = new MinHeap<Integer>(arr);
 		
-		try {
-			minHeap.heapSort();
-			System.out.print(minHeap.getHeap());
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.print("Error");
+		Cat cat = new Cat();
+		
+		for(Method method: cat.getClass().getDeclaredMethods()) {
+			
+			if(method.isAnnotationPresent(ImportantMethod.class)) {
+				Annotation annotation = method.getAnnotation(ImportantMethod.class);
+				
+				for(int i = 0; i < ((ImportantMethod) annotation).times(); i++) {
+					method.invoke(cat);
+				}
+			}
 		}
+//		if(minHeap.getClass().isAnnotationPresent(VeryImportant.class)) {
+//			System.out.println("This is very important");
+//		} else {
+//			System.out.println("This is not very important :(");
+//		}
+		
+//		try {
+//			minHeap.heapSort();
+//			System.out.print(minHeap.getHeap());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			System.out.print("Error");
+//		}
 	}
 }
