9665680708fbd074ccb37edf7b8a5d8fc1ceeb58
ahesh <aheshalagu@gmail.com>
2024-02-13 19:17:35 +0530

    added java file QuickSort.java

---
+++ src/myds/quicksort/QuickSort.java
@@ -0,0 +1,46 @@
+package myds.quicksort;
+
+import java.util.List;
+
+public class QuickSort {
+
+	private void swap(int i, int j, List<Integer> list) throws Exception {
+		Integer iVal = list.get(i);
+		Integer jVal = list.get(j);
+		
+		if(iVal.compareTo(jVal) == 0) {
+			return;
+		}
+		
+		Integer temp = iVal;
+		list.set(i, jVal);
+		list.set(j, temp);
+	}
+	
+	private static int partition(int start, int end, List<Integer> list) {
+		int i = start - 1;
+		
+		int x = list.get(end);
+		
+		for(int j = start; j < end - 1; j++) {
+			if(list.get(j).compareTo(x) > 0) {
+				
+			}
+		}
+		
+		return 0;
+	}
+	
+	private static void quickSortHelper(int start, int end, List<Integer> list) {
+		
+		if(start == end) return;
+		
+		int pivot = partition(start, end, list);
+		quickSortHelper(start, pivot - 1, list);
+		quickSortHelper(pivot + 1, end, list);
+	}
+	
+	public static void quickSort(List<Integer> list) {
+		quickSortHelper(0, list.size() - 1, list);
+	}
+}
