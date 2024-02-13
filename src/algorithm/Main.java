2aac57a7f2f50ab1ed9999b8e8e7e71ddd58ce15
ahesh <aheshalagu@gmail.com>
2024-02-13 19:22:30 +0530

    added condition to if statement

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,27 +1,31 @@
 package algorithm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
 
-import myds.heap.MaxHeap;
-
+//import myds.heap.MinHeap;
+/**
+ * 
+ * @author aheshalagu
+ */
 public class Main {
+	/**
+	 *  This is a test program
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Integer[] arr =  {16, 10, 14, 8, 9, 4, 2, 7, 3, 1};
-//		List<Integer> list = new ArrayList<>(Arrays.asList(arr));
-//		list.set(list.size(), 0);
-//		System.out.println("List: " + list);
 		
-		MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(arr);
-		
-		try {
-//			System.out.println("Heap: " + maxHeap.getHeap());
-			maxHeap.maxHeapInsert(19);
-			System.out.println("Heap: " + maxHeap.getHeap());
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.print("Error");
-		}
+//		MinHeap<Integer> minHeap = new MinHeap<Integer>(arr);
+//		
+//		try {
+//			System.out.println("Heap: " + minHeap.getHeap());
+//			minHeap.heapDelete(1);
+//			System.out.println("Heap: " + minHeap.getHeap());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			System.out.print("Error");
+//		}
 	}
 }
--- src/myds/quicksort/QuickSort.java
+++ src/myds/quicksort/QuickSort.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class QuickSort {
 
-	private void swap(int i, int j, List<Integer> list) throws Exception {
+	private static void swap(int i, int j, List<Integer> list) throws Exception {
 		Integer iVal = list.get(i);
 		Integer jVal = list.get(j);
 		
-		if(iVal.compareTo(jVal) == 0) {
+		if(i == j || iVal.compareTo(jVal) == 0) {
 			return;
 		}
 		
@@ -17,21 +17,22 @@ public class QuickSort {
 		list.set(j, temp);
 	}
 	
-	private static int partition(int start, int end, List<Integer> list) {
+	private static int partition(int start, int end, List<Integer> list) throws Exception{
 		int i = start - 1;
 		
 		int x = list.get(end);
 		
 		for(int j = start; j < end - 1; j++) {
-			if(list.get(j).compareTo(x) > 0) {
-				
+			if(list.get(j).compareTo(x) < 0) {
+				i++;
+				swap(i, j, list);
 			}
 		}
 		
 		return 0;
 	}
 	
-	private static void quickSortHelper(int start, int end, List<Integer> list) {
+	private static void quickSortHelper(int start, int end, List<Integer> list) throws Exception{
 		
 		if(start == end) return;
 		
@@ -40,7 +41,7 @@ public class QuickSort {
 		quickSortHelper(pivot + 1, end, list);
 	}
 	
-	public static void quickSort(List<Integer> list) {
+	public static void quickSort(List<Integer> list) throws Exception {
 		quickSortHelper(0, list.size() - 1, list);
 	}
 }
