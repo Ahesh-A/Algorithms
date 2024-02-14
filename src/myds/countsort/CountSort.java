be5be9f2dbd89271114fddca2a039dbb6c97531d
ahesh <aheshalagu@gmail.com>
2024-02-14 23:49:11 +0530

    added function CountSort

--- src/myds/countsort/CountSort.java
+++ src/myds/countsort/CountSort.java
@@ -9,10 +9,24 @@ public class CountSort {
 		this.arr = arr;
 	}
 	
+	public void calcFrequency(int[] mem, int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			mem[arr[i]]++;
+		}
+	}
+	
+	public void aggregateFrequency(int[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			arr[i] = arr[i] + arr[i - 1];
+		}
+	}
+	
 	public int[] sort() {
 		int[] result = new int[this.arr.length];
+		int[] mem = new int[k + 1];
 		
-		
+		calcFrequency(mem, mem);
+		aggregateFrequency(mem);
 		return result;
 	}
 }
