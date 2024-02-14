54d95d365ac284736ff10d49eb04848f575849ae
ahesh <aheshalagu@gmail.com>
2024-02-14 23:43:27 +0530

    added class CountSort

---
+++ src/myds/countsort/CountSort.java
@@ -0,0 +1,18 @@
+package myds.countsort;
+
+public class CountSort {
+	private int k;
+	private int[] arr;
+	
+	public CountSort(int k, int[] arr) {
+		this.k = k;
+		this.arr = arr;
+	}
+	
+	public int[] sort() {
+		int[] result = new int[this.arr.length];
+		
+		
+		return result;
+	}
+}
