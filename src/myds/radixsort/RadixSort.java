f35e9b1fa8d3f84ece7a15208d8eab0e5c04d7c8
ahesh <aheshalagu@gmail.com>
2024-02-19 19:52:06 +0530

    added function getEmptyList

--- src/myds/radixsort/RadixSort.java
+++ src/myds/radixsort/RadixSort.java
@@ -1,5 +1,30 @@
 package myds.radixsort;
 
-public class RadixSort {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class RadixSort {
+	
+	public Queue<Queue<Integer>> getEmptyList() {
+		Queue<Queue<Integer>> queue = new LinkedList<>();
+		
+		for(int i = 0; i < 10; i++) {
+			queue.offer(new LinkedList<>());
+		}
+		
+		return queue;
+	}
+	
+	public void radixSort(int[] arr) {
+		
+		Queue<Queue<Integer>> queue = getEmptyList();
+		System.out.println(queue);
+		
+		
+	}
+	
+	public void sort(int[] arr) {
+		radixSort(arr);
+		return;
+	}
 }
