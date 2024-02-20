89a74af84e477411c8a2bb731cb31eb64b5ba0f2
ahesh <aheshalagu@gmail.com>
2024-02-20 19:32:20 +0530

    added class Node

---
+++ src/org/ahesh/types/Node.java
@@ -0,0 +1,30 @@
+package org.ahesh.types;
+
+public class Node<T> {
+	private T value;
+	private Node<T> next;
+	
+	public Node() {
+		this.value = null;
+		this.next = null;
+	}
+	
+	public Node(T value) {
+		this.value = value;
+		this.next = null;
+	}
+	
+	public Node(T value, Node<T> next) {
+		this.value = value;
+		this.next = next;
+	}
+	
+	public T value() {
+		return this.value;
+	}
+	
+	public Node<T> next() {
+		return this.next;
+	}
+	
+}
