1f43ef354556b0a4156f2f3eec78be7dc8cfab3f
ahesh <aheshalagu@gmail.com>
2024-02-20 19:45:48 +0530

    added getters and setters

--- src/myds/countsort/LinkedList.java
+++ src/myds/countsort/LinkedList.java
@@ -4,6 +4,19 @@ import org.ahesh.types.Node;
 
 public class LinkedList<T> {
 	Node<T> head;
+
+	public LinkedList(Node<T> head) {
+		super();
+		this.head = head;
+	}
+
+	public Node<T> getHead() {
+		return head;
+	}
+
+	public void setHead(Node<T> head) {
+		this.head = head;
+	}
 	
 	
 }
