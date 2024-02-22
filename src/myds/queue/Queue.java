6c3c6613a6879eb93552f202e7194897ddae9cee
Ahesh-A <aheshalagu@gmail.com>
2024-02-22 23:33:04 +0530

    added function offer in class Queue

--- src/myds/queue/Queue.java
+++ src/myds/queue/Queue.java
@@ -5,41 +5,50 @@ import java.util.Arrays;
 public class Queue<T> {
 	
 	private T[] arr;
-	private int start = 0;
-	private int end = 0;
+	private int head = -1;
+	private int tail = -1;
 	
 	@SuppressWarnings("unchecked")
 	public Queue(int length) {
 		this.arr = (T[])new Object[length];
 	}
 
-	public int getStart() {
-		return start;
+	public int gethead() {
+		return head;
 	}
 
-	public void setStart(int start) {
-		this.start = start;
+	public void sethead(int head) {
+		this.head = head;
 	}
 
-	public int getEnd() {
-		return end;
+	public int gettail() {
+		return tail;
 	}
 
-	public void setEnd(int end) {
-		this.end = end;
+	public void settail(int tail) {
+		this.tail = tail;
 	}
 	
 	public String toString() {
 		return Arrays.toString(this.arr);
 	}
 	
-//	public void offer(T element) throws Exception{
-//		if(arr.length <= Math.abs(end - start) + 1) {
-//			throw new Exception("Queue overflow");
-//		}
-//	}
+	public void offer(T element) throws Exception{
+		if(this.tail == -1) {
+			this.head = 0;
+			this.tail = 0;
+		}
+		
+		this.tail = (this.tail == this.arr.length - 1) ? 0  : this.tail + 1;
+		
+		if(tail == head) {
+			throw new Exception("Queue overflow");
+		}
+		
+		arr[this.tail] = element;
+	}
 	
-//	public void poll() {
-//		
-//	}
+	public void poll() {
+		
+	}
 }
