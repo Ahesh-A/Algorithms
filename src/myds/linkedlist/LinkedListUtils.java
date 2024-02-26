7e35858a6228a9f6697a042dfe4dd351bfc3fd16
Ahesh-A <aheshalagu@gmail.com>
2024-02-26 23:24:58 +0530

    added function printList to class LinkedListUtils

--- src/myds/linkedlist/LinkedListUtils.java
+++ src/myds/linkedlist/LinkedListUtils.java
@@ -7,4 +7,13 @@ public class LinkedListUtils {
 		
 		return new Node<T>();
 	}
+	
+	public <T> void  printList(Node<T> head) {
+		System.out.print("[");
+		while(head != null) {
+			System.out.print(head.value() + " ");
+			head = head.next();
+		}
+		System.out.print("]");
+	}
 }
