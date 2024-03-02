145490584435383b4668c367924415b6ce5d818e
Ahesh-A <aheshalagu@gmail.com>
2024-03-02 22:16:29 +0530

    last commit for class TreeNode

--- src/org/ahesh/types/TreeNode.java
+++ src/org/ahesh/types/TreeNode.java
@@ -22,6 +22,28 @@ public class TreeNode<T> {
 		this.left = null;
 		this.right = null;
 	}
-	
-	
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	public TreeNode<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode<T> left) {
+		this.left = left;
+	}
+
+	public TreeNode<T> getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode<T> right) {
+		this.right = right;
+	}
 }
