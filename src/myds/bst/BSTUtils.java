6ce7124a13ebaefbee06fe003dd6867a3aa8c1b5
ahesh-a <aheshalagu@gmail.com>
2024-03-02 23:46:32 +0530

    added function insert and insertHelper

--- src/myds/bst/BSTUtils.java
+++ src/myds/bst/BSTUtils.java
@@ -4,7 +4,24 @@ import org.ahesh.types.TreeNode;
 
 public class BSTUtils {
 	
-	public static <T> void  insert(TreeNode<T> root, T element) {
+	private static <T extends Comparable<T>> void insertHelper(TreeNode<T> root, TreeNode<T> parent, T element) {
+		if(root == null) {
+			return;
+		}
 		
+		if(root.getValue().compareTo(element) < 0) {
+			insertHelper(root, root.getLeft(), element);
+		}
+			
 	}
+	
+	public static <T extends Comparable<T>> void  insert(TreeNode<T> root, T element) throws Exception{
+		if (root == null) {
+			throw new Exception("The root is null");
+		}
+		
+		insertHelper(root, root, element);
+	}
+	
+	
 }
--- src/myds/bst/BinarySearchTree.java
+++ src/myds/bst/BinarySearchTree.java
@@ -1,5 +1,19 @@
 package myds.bst;
 
-public class BinarySearchTree {
+import org.ahesh.types.TreeNode;
+
+public class BinarySearchTree<T> {
+	private TreeNode<T> root;
 	
+	public BinarySearchTree(TreeNode<T> root) {
+		this.root = root;
+	}
+	
+	public BinarySearchTree(T[] arr) {
+		this.root = new TreeNode<T> (arr[0]);
+		
+		for(int i = 1; i < arr.length; i++) {
+			
+		}
+	}
 }
