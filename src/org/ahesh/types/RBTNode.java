9cd92da978e2eb6f3550b2f51afbe0d897e838dc
ahesh-a <aheshalagu@gmail.com>
2024-03-13 23:38:49 +0530

    added attributes for the class

--- src/org/ahesh/types/RBTNode.java
+++ src/org/ahesh/types/RBTNode.java
@@ -1,5 +1,9 @@
 package org.ahesh.types;
 
-public class RBTNode {
-	
+public class RBTNode<T> {
+	private Color color;
+	private T key;
+	private RBTNode<T> left;
+	private RBTNode<T> right;
+	private RBTNode<T> parent;
 }
