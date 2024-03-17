209d3e6b74bb796d1f443a50553bbf8dd8ac8e97
ahesh-a <aheshalagu@gmail.com>
2024-03-17 23:13:54 +0530

    added parameterized and default constructor

--- src/org/ahesh/types/RBTNode.java
+++ src/org/ahesh/types/RBTNode.java
@@ -3,7 +3,19 @@ package org.ahesh.types;
 public class RBTNode<T> {
 	private Color color;
 	private T key;
+	private RBTNode<T> parent;
 	private RBTNode<T> left;
 	private RBTNode<T> right;
-	private RBTNode<T> parent;
+	
+	public RBTNode() {}
+	
+	public RBTNode(Color color, T key, RBTNode<T> parent, RBTNode<T> left, RBTNode<T> right) {
+		this.color = color;
+		this.key = key;
+		this.parent = parent;
+		this.left = left;
+		this.right = right;
+	}
+	
+	
 }
