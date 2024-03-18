6b463fac4c292e3c534f2fcfea19f82b5f75a8d7
ahesh-a <ahesh.alagu@zohocorp.com>
2024-03-18 23:46:07 +0530

    added getters and setters

--- src/myds/rbt/RBTree.java
+++ src/myds/rbt/RBTree.java
@@ -4,10 +4,12 @@ import org.ahesh.types.RBNode;
 
 public class RBTree<T> {
 	private RBNode<T> root;
-
+	private final RBNode<T> nil = new RBNode<>();
+	
 	public RBTree(RBNode<T> root) {
 		super();
 		this.root = root;
+		
 	}
 	
 	public RBTree(RBNode<T>[] arr) {
@@ -16,5 +18,12 @@ public class RBTree<T> {
 			RBTUtils.insert(root, node);
 		}
 	}
+
+	public RBNode<T> getRoot() {
+		return root;
+	}
+
+	
+	
 	
 }
