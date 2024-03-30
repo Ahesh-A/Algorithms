eef8ae087b997599c03f7b01764bddf126b57299
ahesh-a <aheshalagu@gmail.com>
2024-03-30 16:07:05 +0530

    added function generateNil()

--- src/myds/redblacktree/RBTree.java
+++ src/myds/redblacktree/RBTree.java
@@ -1,24 +1,34 @@
 package myds.redblacktree;
 
 import org.ahesh.types.RBNode;
+import org.ahesh.types.Color;
 
 public class RBTree<T extends Comparable<T>> {
 	private RBNode<T> root;
-	private final RBNode<T> nil = new RBNode<>();
+	private final RBNode<T> nil = generateNil();
+	
 	
 	public RBTree(RBNode<T> root) {
 		super();
 		this.root = root;
-		
 	}
-	
+
+
 	public RBTree(RBNode<T>[] arr) {
 		
 		for(RBNode<T> node: arr) {
 			RBTUtils.insert(this, node);
 		}
 	}
-
+	
+	private RBNode<T> generateNil() {
+		if(this.nil != null) return nil;
+		
+		RBNode<T> rbNode = new RBNode<>();
+		rbNode.setColor(Color.BLACK);
+		return rbNode;
+	}
+	
 	public RBNode<T> getRoot() {
 		return root;
 	}
