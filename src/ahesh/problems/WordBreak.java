b80526e26e673b2f456996e44d573349b3cba18a
ahesh-a <aheshalagu@gmail.com>
2024-04-23 23:38:25 +0530

    added function isWordBreak()

--- src/ahesh/problems/WordBreak.java
+++ src/ahesh/problems/WordBreak.java
@@ -6,7 +6,18 @@ import java.util.Arrays;
 
 public class WordBreak {
 	
-
+	public static boolean isWordBreak(List<String> dictionary, String word) {
+		StringBuilder sb = new StringBuilder(word);
+		
+		for(int i = 0; i <= word.length(); i++) {
+			String subString = sb.substring(0, i);
+			if(dictionary.contains(subString)) {
+				sb.replace(0, i, "");
+			}
+		}
+		return sb.toString().equals("");
+	}
+	
 	public static void main(String[] args) {
 		String word = "applepenapple";
 		List<String> dictionary = Arrays.asList(new String[] {"apple", "pen"});
@@ -14,6 +25,6 @@ public class WordBreak {
 //		System.out.println("Result: " + isWordBreak(dictionary, word));
 		
 		StringBuilder sb = new StringBuilder(word);
-		System.out.println(sb.substring(0, word.le));
+//		System.out.println(sb.substring(0, word.le));
 	}
 }
