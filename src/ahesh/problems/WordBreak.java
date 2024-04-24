9f0e6ac11bcee76fe0ff3b7198953db499186e78
ahesh-a <aheshalagu@gmail.com>
2024-04-24 23:41:16 +0530

    replaced word with sb and i=0

--- src/ahesh/problems/WordBreak.java
+++ src/ahesh/problems/WordBreak.java
@@ -9,22 +9,25 @@ public class WordBreak {
 	public static boolean isWordBreak(List<String> dictionary, String word) {
 		StringBuilder sb = new StringBuilder(word);
 		
-		for(int i = 0; i <= word.length(); i++) {
+		for(int i = 1; i <= sb.length(); i++) {
 			String subString = sb.substring(0, i);
 			if(dictionary.contains(subString)) {
 				sb.replace(0, i, "");
+				i = 0;
 			}
 		}
 		return sb.toString().equals("");
 	}
 	
 	public static void main(String[] args) {
-		String word = "applepenapple";
-		List<String> dictionary = Arrays.asList(new String[] {"apple", "pen"});
+		String word = "catsanddog";
+		List<String> dictionary = Arrays.asList(new String[] {"cats","dog","sand","and","cat"});
 		
-//		System.out.println("Result: " + isWordBreak(dictionary, word));
+		System.out.println("Result: " + isWordBreak(dictionary, word));
 		
-		StringBuilder sb = new StringBuilder(word);
-//		System.out.println(sb.substring(0, word.le));
+//		StringBuilder sb = new StringBuilder(word);
+//		sb.replace(0, 3, "");
+//		
+//		System.out.println(sb);
 	}
 }
