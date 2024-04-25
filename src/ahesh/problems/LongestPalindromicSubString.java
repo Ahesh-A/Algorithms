69b054d67a64c8cc334ffda710e5279fcb1737b4
ahesh-a <aheshalagu@gmail.com>
2024-04-25 23:30:00 +0530

    added function main and initialized variables

--- src/ahesh/problems/LongestPalindromicSubString.java
+++ src/ahesh/problems/LongestPalindromicSubString.java
@@ -1,5 +1,20 @@
+//Given a string s, return the longest palindromic substring in s.
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "babad"
+//Output: "bab"
+//Explanation: "aba" is also a valid answer.
+
 package ahesh.problems;
 
 public class LongestPalindromicSubString {
-
+	
+	
+	public static void main(String[] args) {
+		String charSeq = "babad";
+		System.out.println("Result: " + getLongestPalindromicSubString(charSeq));
+	}
 }
