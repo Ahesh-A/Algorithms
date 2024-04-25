ccd2e170352852084cc9aa0ca64888c779289f03
ahesh-a <aheshalagu@gmail.com>
2024-04-25 23:51:48 +0530

    implemented function getLongestPalindromicSubString

--- src/ahesh/problems/LongestPalindromicSubString.java
+++ src/ahesh/problems/LongestPalindromicSubString.java
@@ -13,12 +13,42 @@ package ahesh.problems;
 public class LongestPalindromicSubString {
 	
 	public static String getLongestPalindromicSubString(String charSeq) {
+		String result = "";
+		int start;
+		int end;
 		
-		return "";
+		for(int i = 0; i < charSeq.length(); i++) {
+			start  = i;
+			end = i;
+			
+			while(start >= 0 && end < charSeq.length()) {
+				if(charSeq.charAt(start) == charSeq.charAt(end)) {
+					if(end - start + 1 > result.length()) {
+						result = charSeq.substring(start, end + 1);
+					}
+					start --;
+					end ++;
+				} else break;
+			}
+			
+			start = i;
+			end = i + 1;
+			
+			while(start >= 0 && end < charSeq.length()) {
+				if(charSeq.charAt(start) == charSeq.charAt(end)) {
+					if(end - start + 1 > result.length()) {
+						result = charSeq.substring(start, end + 1);
+					}
+					start --;
+					end ++;
+				} else break;
+			}
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
-		String charSeq = "babad";
+		String charSeq = "maam";
 		System.out.println("Result: " + getLongestPalindromicSubString(charSeq));
 	}
 }
