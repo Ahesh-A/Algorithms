dba7931ba152eb4ea21c2711a33479a7580c16b4
ahesh-a <aheshalagu@gmail.com>
2024-04-27 00:45:07 +0530

    addded function validateString

--- src/ahesh/problems/DecodeWays.java
+++ src/ahesh/problems/DecodeWays.java
@@ -2,7 +2,17 @@ package ahesh.problems;
 
 public class DecodeWays {
 	
+	public static boolean validateString(String s) {
+		return s.compareTo("0") > 0 && s.compareTo("26") <= 0;
+	}
+	
 	public static int noOfWaysToDecode(String s) {
+		int result = 0;
+		// sepearte single 
+		if(validateString(s.substring(0, 1))) {
+			result += noOfWaysToDecode(s.substring(1, s.length()));
+		}
+		result += noOfWaysToDecode(s);
 		return 0;
 	}
 	
