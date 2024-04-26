bce00677c65ff53d90250cfc410c467f34b085f6
ahesh-a <aheshalagu@gmail.com>
2024-04-27 00:29:45 +0530

    initialized  function noOfWaysToDecode

--- src/ahesh/problems/DecodeWays.java
+++ src/ahesh/problems/DecodeWays.java
@@ -2,9 +2,9 @@ package ahesh.problems;
 
 public class DecodeWays {
 	
-//	public static int noOfWaysToDecode(String s) {
-//		return 0
-//	}
+	public static int noOfWaysToDecode(String s) {
+		return 0;
+	}
 	
 	public static void main(String[] args) {
 		String s = "";
