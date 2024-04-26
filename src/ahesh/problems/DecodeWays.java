f50d3fad97585db77252a20e761eae092a5ff90b
ahesh-a <aheshalagu@gmail.com>
2024-04-27 00:28:20 +0530

    added function main

--- src/ahesh/problems/DecodeWays.java
+++ src/ahesh/problems/DecodeWays.java
@@ -1,5 +1,13 @@
 package ahesh.problems;
 
 public class DecodeWays {
-
+	
+//	public static int noOfWaysToDecode(String s) {
+//		return 0
+//	}
+	
+	public static void main(String[] args) {
+		String s = "";
+		System.out.println("Result: " + noOfWaysToDecode(s));
+	}
 }
