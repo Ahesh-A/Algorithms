067d24d4998d0a5a9978c6bdbaf6db6df3dea84d
ahesh-a <aheshalagu@gmail.com>
2024-04-27 13:43:20 +0530

    introduced cache to optimize function

--- src/ahesh/problems/DecodeWays.java
+++ src/ahesh/problems/DecodeWays.java
@@ -1,46 +1,49 @@
 package ahesh.problems;
 
+import java.util.HashMap;
+
 public class DecodeWays {
 	
 	public static boolean validateString(String s) {
+		
 		return s.charAt(0) != '0' &&  Integer.parseInt(s) > 0 && Integer.parseInt(s) <= 26;
 	}
 	
-	public static int singleSlice(String s) {
+	public static int singleSlice(String s, HashMap<String, Integer> cache) {
 		
 		if(s.length() >= 1 && validateString(s.substring(0, 1))) {
-//			System.out.println("Validated for length 1");
-			return noOfWaysToDecode(s.substring(1, s.length()));
+			
+			return noOfWaysToDecode(s.substring(1, s.length()), cache);
 		}
 		
 		return 0;
 		
 	}
 	
-	public static int doubleSlice(String s) {
+	public static int doubleSlice(String s, HashMap<String, Integer> cache) {
 		if(s.length() >= 2 && validateString(s.substring(0, 2))) {
-//			System.out.println("Validated for length 245w");
-			return noOfWaysToDecode(s.substring(2, s.length()));
+			
+			return noOfWaysToDecode(s.substring(2, s.length()), cache);
 		}
 		
 		return 0;
 	}
 	
-	public static int noOfWaysToDecode(String s) {
+	public static int noOfWaysToDecode(String s, HashMap<String, Integer> cache) {
 		
-		System.out.println("String :" + s);
+		if(cache.containsKey(s)) return cache.get(s);
+		
 		if(s.equals("")) return 1;	
 		
+		int res = singleSlice(s, cache) + doubleSlice(s, cache);
+		cache.put(s, res);
 		
-		return singleSlice(s) + doubleSlice(s);
-		
-		
+		return res;
 	}
 	
 	public static void main(String[] args) {
-		String s = "11106";
-		System.out.println("Result: " + noOfWaysToDecode(s));
+		String s = "111111111111111111111111111111111111111111111"; //1836311903
+		System.out.println("Result: " + noOfWaysToDecode(s, new HashMap<String, Integer>()));
 		
-//		System.out.println("Result: " + singleSlice("6"));
 	}
 }
