0e878293d500db4abf648da366e80ba6c11dfde2
ahesh-a <aheshalagu@gmail.com>
2024-05-03 23:55:56 +0530

    initialized function canJump

--- src/ahesh/problems/JumpGame.java
+++ src/ahesh/problems/JumpGame.java
@@ -1,6 +1,9 @@
 package ahesh.problems;
 
 public class JumpGame {
+	public static boolean canJump(int[] arr) {
+		
+	}
 	public static void main(String[] args) {
 		int[] arr = {2, 3, 1, 1, 4};
 		
