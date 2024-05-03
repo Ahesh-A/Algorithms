56512ef0244b7c3c280cfe6c24582ccf73440b3b
ahesh-a <aheshalagu@gmail.com>
2024-05-03 23:58:51 +0530

    defined function canJump

--- src/ahesh/problems/JumpGame.java
+++ src/ahesh/problems/JumpGame.java
@@ -2,10 +2,20 @@ package ahesh.problems;
 
 public class JumpGame {
 	public static boolean canJump(int[] arr) {
+		int max = arr[0];
 		
+		for(int i = 0; i < max; i++) {
+			max = Integer.max(max, i + arr[i]);
+			if(max >= arr.length - 1) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
+	
 	public static void main(String[] args) {
-		int[] arr = {2, 3, 1, 1, 4};
-		
+		int[] arr = {1, 0, 1, 0};
+		System.out.println("Result: " + canJump(arr));
 	}
 }	
