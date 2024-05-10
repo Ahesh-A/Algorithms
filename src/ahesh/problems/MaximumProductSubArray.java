d1ac4695ee0fedb4c4b34633d72b7209269d593a
ahesh-a <aheshalagu@gmail.com>
2024-05-10 08:23:56 +0530

    defined function maxProduct

--- src/ahesh/problems/MaximumProductSubArray.java
+++ src/ahesh/problems/MaximumProductSubArray.java
@@ -1,7 +1,25 @@
 package ahesh.problems;
 
 public class MaximumProductSubArray {
-	public static void main(String[] args) {
+	
+	public static int maxProduct(int[] arr) {
+		int curMax = 1;
+		int curMin = 1;
+		int res = arr[0];
+		for(int i : arr) {
+			int max = i * curMax;
+			int min = i * curMin;
+			curMax = Integer.max(Integer.max(max, min), i);
+			curMin = Integer.max(Integer.max(max, min), i);
+			
+			res = Integer.max(Integer.max(curMax, curMin), res);
+		}
 		
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {2, 3, -2, 4};
+		System.out.println("Result: " + maxProduct(arr));
 	}
 }
