9c068dabf1bacdc7fe6e038233f6daf12e04b02a
ahesh-a <aheshalagu@gmail.com>
2024-05-10 23:13:41 +0530

    initialized function minCoins

--- src/ahesh/problems/CoinChange.java
+++ src/ahesh/problems/CoinChange.java
@@ -1,5 +1,15 @@
 package ahesh.problems;
 
 public class CoinChange {
-
+	
+	public static int minCoins(int[] coins, int target) {
+		
+	}
+	
+	public static void main(String[] rags) {
+		int[] coins = {1, 2, 5};
+		int target = 11;
+		
+		
+	}
 }
