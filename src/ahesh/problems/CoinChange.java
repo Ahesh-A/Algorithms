4b05c9396fbacde720bfc84b3c1654221b25b91b
ahesh-a <aheshalagu@gmail.com>
2024-05-10 23:27:24 +0530

    defined function minCoins

--- src/ahesh/problems/CoinChange.java
+++ src/ahesh/problems/CoinChange.java
@@ -3,13 +3,25 @@ package ahesh.problems;
 public class CoinChange {
 	
 	public static int minCoins(int[] coins, int target) {
+		if(target == 0) return 0;
 		
+		int noOfWays = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < coins.length; i++) {
+			int temp = target - coins[i];
+			
+			if(temp >=0) {
+				noOfWays = Integer.min(noOfWays, minCoins(coins, temp) + 1);
+			}
+		}
+		
+		return noOfWays;
 	}
 	
 	public static void main(String[] rags) {
 		int[] coins = {1, 2, 5};
 		int target = 11;
 		
-		
+		System.out.println("Result: " + minCoins(coins, target));
 	}
 }
