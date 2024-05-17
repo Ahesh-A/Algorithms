419beec10a965aca02c3d22fa3e216c681e55e46
ahesh-a <aheshalagu@gmail.com>
2024-05-17 22:56:16 +0530

    defined function getMinNeighbourhood

--- src/ahesh/problems/MaximalSquare.java
+++ src/ahesh/problems/MaximalSquare.java
@@ -7,11 +7,28 @@ package ahesh.problems;
 
 public class MaximalSquare {
 	
-	public static void getMinNeighbourhood(int rowIdx, int colIdx) {
+	public static int getMinNeighbourhood(int rowIdx, int colIdx, int[][] arr ) {
+		final int rowLength = arr.length;
+		final int colLength = arr[0].length;
 		
+		int max = 0;
+		
+		if(colIdx + 1 < colLength && rowIdx < rowLength) {
+			max = Integer.max(max, arr[rowIdx][colIdx + 1]);
+		}
+		
+		if(rowIdx + 1 < rowLength && colIdx < colLength) {
+			max = Integer.max(max, arr[rowIdx][colIdx]);
+		}
+		
+		if(rowIdx + 1 < rowLength && colIdx + 1 < colLength) {
+			max = Integer.max(max, arr[rowIdx][colIdx]);
+		}
+		
+		return max;
 	}
 	
-	public static void fillRow(int rowIdx, int colIdx) {
+	public static void fillRow(int rowIdx, int colIdx, int[][] arr) {
 		for(int j = colIdx; j >= 0; j--) {
 			
 		}
