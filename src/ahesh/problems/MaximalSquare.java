f87bd98fc9f6b7b2ede0380d7e6645fa96e7dd28
ahesh-a <aheshalagu@gmail.com>
2024-05-18 00:01:24 +0530

    fixed function getMinNeighbourhood and insertHandler

--- src/ahesh/problems/MaximalSquare.java
+++ src/ahesh/problems/MaximalSquare.java
@@ -9,60 +9,63 @@ import java.util.Arrays;
 
 public class MaximalSquare {
 	
-	public static void insertHandler(int rowIdx, int colIdx, char[][] arr) {
-		if(arr[rowIdx][colIdx] != '0') {
-			arr[rowIdx][colIdx] = (char)(getMinNeighbourhood(rowIdx, colIdx, arr) + 1);
+	public static int insertHandler(int rowIdx, int colIdx, char[][] arr) {
+		int minNeighbourhood = getMinNeighbourhood(rowIdx, colIdx, arr);
+		
+		if(arr[rowIdx][colIdx] != '0' && minNeighbourhood != 0) {
+			arr[rowIdx][colIdx] += minNeighbourhood;
 		}
+	
+		return arr[rowIdx][colIdx] - '0';
 	}
 	
 	public static int getMinNeighbourhood(int rowIdx, int colIdx, char[][] arr ) {
 		final int rowLength = arr.length;
 		final int colLength = arr[0].length;
 		
+		if(colIdx + 1 == colLength || rowIdx+ 1 == rowLength) return 0;
+		
+		int min = Integer.max(rowLength, colLength);
+		
+		min = Integer.min(min, arr[rowIdx][colIdx + 1] - '0');
+		min = Integer.min(min, arr[rowIdx + 1][colIdx] - '0');
+		min = Integer.min(min, arr[rowIdx + 1][colIdx + 1] - '0');
+		
+		
+		return min;
+	}
+	
+	public static int fillRow(int rowIdx, int colIdx, char[][] arr) {
 		int max = 0;
-		
-		if(colIdx + 1 < colLength && rowIdx < rowLength) {
-			max = Integer.max(max, arr[rowIdx][colIdx + 1] - '0');
-		}
-		
-		if(rowIdx + 1 < rowLength && colIdx < colLength) {
-			max = Integer.max(max, arr[rowIdx][colIdx] - '0');
-		}
-		
-		if(rowIdx + 1 < rowLength && colIdx + 1 < colLength) {
-			max = Integer.max(max, arr[rowIdx][colIdx] - '0');
+		for(int j = colIdx; j >= 0; j--) {
+			max  = Integer.max(max, insertHandler(rowIdx, j, arr));
 		}
 		
 		return max;
 	}
 	
-	public static void fillRow(int rowIdx, int colIdx, char[][] arr) {
-		for(int j = colIdx; j >= 0; j--) {
-			
-		}
+	public static int getMaximalSquare(char[][] arr) {
+		int max = 0;
+		final int row = arr.length;
+		final int col = arr[0].length;
+		
+		for(int i = row - 1; i >= 0; i--) {
+			max = Integer.max(max, fillRow(i, col - 1, arr));
+			System.out.println("max: " + max);
+		} 
+		
+		return max;
 	}
-	
-//	public static int getMaximalSquare(char[][] arr) {
-//		int max = 0;
-//		final int row = arr.length;
-//		final int col = arr[0].length;
-//		
-//		for(int i = row - 1; i >= 0; i--) {
-//			
-//		}
-//		return max
-//	}
 	
 	public static void main(String[] args) {
 		char [][] arr = {
 				{'1','0','1','0','0'},
-				{'1','0','1','1','1'},
+				{'1','0','1','0','1'},
 				{'1','1','1','1','1'},
-				{'1','0','0','1','0'}
+				{'1','0','1','1','1'}
 		};
 		
-//		System.out.println(Arrays.deepToString(arr));
-		System.out.println((char)('1' + 1));
-//		System.out.println("Result: " + getMaximalSquare(arr));
+		System.out.println("Result: " + getMaximalSquare(arr));
+		System.out.println(Arrays.deepToString(arr));
 	}
 }
