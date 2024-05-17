a4dbca280e0c32ff5b06ba3582f054043b2c24ea
ahesh-a <aheshalagu@gmail.com>
2024-05-17 22:34:17 +0530

    added function main

--- src/ahesh/problems/MaximalSquare.java
+++ src/ahesh/problems/MaximalSquare.java
@@ -1,5 +1,19 @@
+//Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+//Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+//Output: 4
+
 package ahesh.problems;
 
 public class MaximalSquare {
-	
+	public static void main(String[] args) {
+		String[][] arr = {
+				{"1","0","1","0","0"},
+				{"1","0","1","1","1"},
+				{"1","1","1","1","1"},
+				{"1","0","0","1","0"}
+		};
+		
+		System.out.println("REsult: " + getMaximalSquare(arr));
+	}
 }
