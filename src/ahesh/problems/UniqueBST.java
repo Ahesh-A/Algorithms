f65eb5121c918dc8391024f84f14c8b93ce65050
ahesh-a <aheshalagu@gmail.com>
2024-05-26 20:16:54 +0530

    added function getNumTrees

--- src/ahesh/problems/UniqueBST.java
+++ src/ahesh/problems/UniqueBST.java
@@ -8,8 +8,29 @@
 package ahesh.problems;
 
 public class UniqueBST {
+	
+	public static int getNumTrees(int sum, int i, int[] subFactor) {
+		
+		
+		if(iMod2 == 1) {
+			int result = 2 * sum - subFactor[iMod2];
+			return result;
+		}
+		return 2 * sum;
+	}
+	
 	public static int numTrees(int n) {
-	        
+		if(n <= 1) return n;
+		
+		int sum = 1;
+		int[] subFactor = new int[n / 2 + 1];
+		
+		for(int i = 2; i <= n; i++) {
+			sum += getNumTrees(sum, i , subFactor);
+			updateSubFactor(sum, subFactor);
+		}
+		
+		return sum;
 	}
 	
 	public static void main(String[] args) {
