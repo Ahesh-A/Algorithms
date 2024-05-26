a74c6b3646cafd5b9016f914f885b08fc23f72af
ahesh-a <aheshalagu@gmail.com>
2024-05-26 21:23:00 +0530

    added function fillDp

--- src/ahesh/problems/UniqueBST.java
+++ src/ahesh/problems/UniqueBST.java
@@ -9,32 +9,42 @@ package ahesh.problems;
 
 public class UniqueBST {
 	
+	public static void updateSubFactor(int sum, int idx, int num,  int[] subFactor) {
+		if(idx <= num / 2) {
+			subFactor[idx] = sum;
+		}
+	}
+	
 	public static int getNumTrees(int sum, int i, int[] subFactor) {
 		
-		
-		if(iMod2 == 1) {
-			int result = 2 * sum - subFactor[iMod2];
-			return result;
+		if(i % 2 == 1) {
+			return 2 * sum - subFactor[i / 2];
 		}
 		return 2 * sum;
+	}
+	
+	public static void fillDp(int idx, int[] dp) {
+		for(int i = 1; i <= idx; i++) {
+			dp[idx] += dp[Integer.max(i - 1, idx - i)];
+		}
 	}
 	
 	public static int numTrees(int n) {
 		if(n <= 1) return n;
 		
-		int sum = 1;
-		int[] subFactor = new int[n / 2 + 1];
+		int[] dp = new int[n + 1];
 		
-		for(int i = 2; i <= n; i++) {
-			sum += getNumTrees(sum, i , subFactor);
-			updateSubFactor(sum, subFactor);
+		dp[1] = 1;
+		
+		for(int i = 1; i <= n; i++) {
+			fillDp(i, dp);
 		}
 		
-		return sum;
+		return dp[n];
 	}
 	
 	public static void main(String[] args) {
-		int num = 3;
+		int num = 5;
 		
 		System.out.println("Result: " + numTrees(num));
 	}
