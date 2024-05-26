b367e3c2aa10fa2bdaf2c0dc2072d85437526508
ahesh-a <aheshalagu@gmail.com>
2024-05-26 19:35:32 +0530

    added function main

---
+++ src/ahesh/problems/UniqueBST.java
@@ -0,0 +1,21 @@
+//Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+//
+//Example 1:
+//
+//Input: n = 3
+//Output: 5
+
+package ahesh.problems;
+
+public class UniqueBST {
+//	public static int numTrees(int n) {
+//	        
+//	}
+	
+	public static void main(String[] args) {
+		int num = 3;
+		
+		System.out.println("Result: " + numTrees(num));
+	}
+	   
+}
