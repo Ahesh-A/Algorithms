ee49d45726f67268fc2f519580610be2d7f91a16
ahesh-a <aheshalagu@gmail.com>
2024-05-28 23:04:40 +0530

    commented function fillIdWithVal

--- src/ahesh/problems/Jump2.java
+++ src/ahesh/problems/Jump2.java
@@ -15,19 +15,36 @@
 //Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
 package ahesh.problems;
 
+import java.util.Arrays;
+
 public class Jump2 {
-	public static void fillIdxWithVal(int[] temp, int val, int startIdx, int endIdx) {
-		for(int i = startIdx; i <= endIdx; i++) {
-			temp[i] = val;
-		}
-	}
+//	public static void fillIdxWithVal(int[] temp, int val, int startIdx, int endIdx) {
+//		for(int i = startIdx; i <= endIdx; i++) {
+//			temp[i] = val;
+//		}
+//		
+//		System.out.println(Arrays.toString(temp));
+//	}
 	public static int getMinJumps(int[] arr) {
-		int[] jumpArr = new int[arr.length];
+//		int[] jumpArr = new int[arr.length];
+		int vistedIdx = 0;
+		int step = 0;
 		
-		return jumpArr[arr.length - 1];
+		for(int i = 0; i < arr.length; i++) {
+			int tempIdx = Integer.min(arr.length - 1, i + arr[i]);
+			if(vistedIdx >= arr.length - 1) return step;
+			
+//			if(vistedIdx < tempIdx) {
+//				fillIdxWithVal(jumpArr, step, vistedIdx + 1, tempIdx);
+				vistedIdx = tempIdx;
+				step++;
+//			}
+		}
+		
+		return step; //jumpArr[arr.length - 1];
 	}
 	public static void main(String[] args) {
-		int[] jump = {2, 3, 1, 1, 4};
+		int[] jump = {1, 5, 2, 1, 4, 5, 7, 3}; // [1, 5, 2, 1, 4, 5, 7, 3] ans: 3
 		System.out.println("Result: " + getMinJumps(jump));
 	}
 }
