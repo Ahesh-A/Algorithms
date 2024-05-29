8c1784d1c378b5a20d4069d80ea9838769cc01dc
ahesh-a <aheshalagu@gmail.com>
2024-05-29 07:44:06 +0530

    solved Jump2

--- src/ahesh/problems/Jump2.java
+++ src/ahesh/problems/Jump2.java
@@ -18,33 +18,33 @@ package ahesh.problems;
 import java.util.Arrays;
 
 public class Jump2 {
-//	public static void fillIdxWithVal(int[] temp, int val, int startIdx, int endIdx) {
-//		for(int i = startIdx; i <= endIdx; i++) {
-//			temp[i] = val;
-//		}
-//		
-//		System.out.println(Arrays.toString(temp));
-//	}
-	public static int getMinJumps(int[] arr) {
-//		int[] jumpArr = new int[arr.length];
-		int vistedIdx = 0;
-		int step = 0;
-		
-		for(int i = 0; i < arr.length; i++) {
-			int tempIdx = Integer.min(arr.length - 1, i + arr[i]);
-			if(vistedIdx >= arr.length - 1) return step;
-			
-//			if(vistedIdx < tempIdx) {
-//				fillIdxWithVal(jumpArr, step, vistedIdx + 1, tempIdx);
-				vistedIdx = tempIdx;
-				step++;
-//			}
+	public static void fillIdxWithVal(int[] temp, int val, int startIdx, int endIdx) {
+		for(int i = startIdx; i <= endIdx; i++) {
+			temp[i] = val;
 		}
 		
-		return step; //jumpArr[arr.length - 1];
+		System.out.println(Arrays.toString(temp));
+	}
+	public static int getMinJumps(int[] arr) {
+		int[] jumpArr = new int[arr.length];
+		int visitedIdx = 0;
+//		int step = 0;
+		
+		for(int i = 0; i <= visitedIdx; i++) {
+			int tempIdx = Integer.min(arr.length - 1, i + arr[i]);
+//			if(vistedIdx >= arr.length - 1) return step;
+			
+			if(visitedIdx < tempIdx) {
+				fillIdxWithVal(jumpArr, jumpArr[i] + 1, visitedIdx + 1, tempIdx);
+				visitedIdx = tempIdx;
+//				step++;
+			}
+		}
+		
+		return jumpArr[arr.length - 1];
 	}
 	public static void main(String[] args) {
-		int[] jump = {1, 5, 2, 1, 4, 5, 7, 3}; // [1, 5, 2, 1, 4, 5, 7, 3] ans: 3
+		int[] jump = {7,0,9,6,9,6,1,7,9,0,1,2,9,0,3}; // [7,0,9,6,9,6,1,7,9,0,1,2,9,0,3] ans: 2
 		System.out.println("Result: " + getMinJumps(jump));
 	}
 }
