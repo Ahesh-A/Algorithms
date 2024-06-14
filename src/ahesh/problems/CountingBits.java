2b4c1938cad546571c8df74c49967ba466c3fad1
Ahesh-A <aheshalagu@gmail.com>
2024-06-14 22:54:15 +0530

    added function getCountArr

--- src/ahesh/problems/CountingBits.java
+++ src/ahesh/problems/CountingBits.java
@@ -24,16 +24,23 @@
 
 package ahesh.problems;
 
+import java.util.Arrays;
+
 public class CountingBits {
 	
 	public static int[] getCountArr(int n) {
 		int[] arr = new int[n + 1];
+		arr[0] = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			arr[i] = (i & 1) + arr[i / 2]; 
+		}
 		
 		return arr;
 	}
 	
 	public static void main(String[] args) {
-		int n = 5;
-		
+		int n = 2;
+		System.out.println("Result: " + Arrays.toString(getCountArr(n)));
 	}
 }
