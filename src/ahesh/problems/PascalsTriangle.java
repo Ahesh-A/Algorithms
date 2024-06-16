7dbcd7a71a6e9337a9e01c8964a15aad1b199d50
ahesh-a <aheshalagu@gmail.com>
2024-06-16 21:14:37 +0530

    added function generate

--- src/ahesh/problems/PascalsTriangle.java
+++ src/ahesh/problems/PascalsTriangle.java
@@ -1,5 +1,17 @@
 package ahesh.problems;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class PascalsTriangle {
-	
+	public List<List<Integer>> generate(int numRows) {
+		 List<List<Integer>> pascalsTrinagle = new ArrayList<>();
+		 
+		 return pascalsTrinagle;
+	}	
+	 
+	public static void main(String[] args) {
+		int num = 5;
+		
+	}
 }
