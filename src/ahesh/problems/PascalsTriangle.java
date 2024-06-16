e7d59992155f304885405a6838108dda5bccce95
ahesh-a <aheshalagu@gmail.com>
2024-06-16 21:32:13 +0530

    defined function getCurrentPascalRow

--- src/ahesh/problems/PascalsTriangle.java
+++ src/ahesh/problems/PascalsTriangle.java
@@ -4,14 +4,38 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class PascalsTriangle {
-	public List<List<Integer>> generate(int numRows) {
+	public static List<Integer> getCurrentPascalRow(List<Integer> row) {
+		Integer i = -1;
+		Integer j = 0;
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for(; i < row.size(); i++, j++) {
+			if(i < 0 || j == row.size()) {
+				list.add(1);
+			} else {
+				list.add(row.get(i) + row.get(j));
+			}
+		}
+		
+		return list;
+	}
+	public static  List<List<Integer>> generate(int numRows) {
 		 List<List<Integer>> pascalsTrinagle = new ArrayList<>();
+		 List<Integer> initialList = new ArrayList<Integer>();
+		 initialList.add(1);
+		 pascalsTrinagle.add(initialList);
 		 
+		 List<Integer> previousList = initialList;
+		 
+		 for(int i = 1; i < numRows; i++) {
+			 previousList =  getCurrentPascalRow(previousList);
+			 pascalsTrinagle.add(previousList);
+		 }
 		 return pascalsTrinagle;
 	}	
 	 
 	public static void main(String[] args) {
 		int num = 5;
-		
+		System.out.println("Result: " + generate(num));
 	}
 }
