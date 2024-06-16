d21d65dac7868c528aedd0c726946815ec234609
ahesh-a <aheshalagu@gmail.com>
2024-06-16 21:54:52 +0530

    added function getPascalRowValue

--- src/ahesh/problems/PascalsTriangle.java
+++ src/ahesh/problems/PascalsTriangle.java
@@ -4,21 +4,28 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class PascalsTriangle {
+	public static Integer getPascalRowValue(int i, int j, int rowSize, List<Integer> row) {
+		if(i < 0 || j == rowSize) {
+			return 1;
+		}
+		
+		return row.get(i) + row.get(j);
+		
+	}
 	public static List<Integer> getCurrentPascalRow(List<Integer> row) {
 		Integer i = -1;
 		Integer j = 0;
 		List<Integer> list = new ArrayList<Integer>();
 		
-		for(; i < row.size(); i++, j++) {
-			if(i < 0 || j == row.size()) {
-				list.add(1);
-			} else {
-				list.add(row.get(i) + row.get(j));
-			}
+		int rowSize = row.size();
+		
+		for(; i < rowSize; i++, j++) {
+			list.add(getPascalRowValue(i, j, rowSize, row));
 		}
 		
 		return list;
 	}
+	
 	public static  List<List<Integer>> generate(int numRows) {
 		 List<List<Integer>> pascalsTrinagle = new ArrayList<>();
 		 List<Integer> initialList = new ArrayList<Integer>();
