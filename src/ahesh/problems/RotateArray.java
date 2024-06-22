224231bef98bbb7bb4321fcb2799d2935fe7e45a
ahesh-a <aheshalagu@gmail.com>
2024-06-22 22:42:16 +0530

    added function rotateArray

--- src/ahesh/problems/RotateArray.java
+++ src/ahesh/problems/RotateArray.java
@@ -4,22 +4,22 @@ import java.util.Arrays;
 
 public class RotateArray {
 	
-//	public static void rotateArray(int[] arr, int rotationFactor) {
-//		int arrLen = arr.length;
-//		
-//		int r = 0, k, rval = arr[r], kval;
-//		
-//		for(int i = 0; i < arr.length; i++) {
-//			k = (r + rotationFactor) % arrLen;
-//			kval = arr[k];
-//			arr[k] = rval;
-//			rval = kval;
-//			r = k;
-//			
-//			System.out.println("Result: " + Arrays.toString(arr));
-//		}
-//		
-//	}
+	public static void rotateArray(int[] arr, int rotationFactor) {
+		int arrLen = arr.length;
+		
+		int r = 0, k, rval = arr[r], kval;
+		
+		for(int i = 0; i < arr.length; i++) {
+			k = (r + rotationFactor) % arrLen;
+			kval = arr[k];
+			arr[k] = rval;
+			rval = kval;
+			r = k;
+			
+			System.out.println("Result: " + Arrays.toString(arr));
+		}
+		
+	}
 	
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3, 4, 5, 6, 7};
