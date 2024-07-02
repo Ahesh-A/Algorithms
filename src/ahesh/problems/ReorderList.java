5ac403674065f883c40507b6dac58e81a7ec4015
Ahesh-A <aheshalagu@gmail.com>
2024-07-02 23:23:33 +0530

    added function constructLinkedLis

--- src/ahesh/problems/ReorderList.java
+++ src/ahesh/problems/ReorderList.java
@@ -27,18 +27,37 @@
 
 package ahesh.problems;
 
-public class ReorderList {
+class ListNode{
+	ListNode next;
+	int val;
 	
-	class ListNode{
-		ListNode next;
-		int val;
+	ListNode(){}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReorderList {
 		
-		ListNode(){}
-		ListNode(int val) { this.val = val; }
-		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	public static ListNode constructLinkedList(int[] arr) {
+		ListNode head = new ListNode();
+		ListNode temp = head;
+		for(int i : arr) {
+			ListNode node = new ListNode(i);
+			temp.next = node;
+			temp = node;
+		}
+		
+		return head.next;
 	}
+//	public static void printList(ListNode head) {
+//		while(head != null) {
+//			System.out.println(head.val);
+//			head = head.next;
+//		}
+//	}
 	
 	public static void main(String[] args) {
-		
+		ListNode linkedList = constructLinkedList(new int[] {1, 2, 3, 4, 5, 6, 7 });
+		printList(linkedList);
 	}
 }
