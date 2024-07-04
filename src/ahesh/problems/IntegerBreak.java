98a29467df098c1ad5c2a72f301937b797a358c0
Ahesh-A <aheshalagu@gmail.com>
2024-07-04 22:48:17 +0530

    added function integerBreakDP

--- src/ahesh/problems/IntegerBreak.java
+++ src/ahesh/problems/IntegerBreak.java
@@ -21,12 +21,41 @@
 //2 <= n <= 58
 package ahesh.problems;
 
+import java.util.Arrays;
+
 public class IntegerBreak {
+	public static int integerBreakDP(int n, int[] dp) {
+		
+		if(dp[n] != 0) {
+			return dp[n];
+		}
+		
+		for(int i = 1; i <= n / 2; i++) {			
+			dp[n] = Integer.max(i * integerBreakDP(n - i, dp), dp[n]);
+		}
+		
+		return dp[n];
+	}
 	
 	public static int integerBreak(int n) {
-	        
-	}
-	public static void main(String[] args) {
+		if(n == 1 || n == 2) {
+			return 1;
+		}
+		if(n == 3) {
+			return 2;
+		}
 		
+		int[] dp = new int[n + 1];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 3;
+		
+		return integerBreakDP(n, dp);
+	}
+	
+	public static void main(String[] args) {
+		int res = integerBreak(58);
+		System.out.println("Result: " + res);
 	}
 }
