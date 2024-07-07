835530d83f0e72abc975b90ee6759531754d0d72
Ahesh-A <aheshalagu@gmail.com>
2024-07-07 19:45:02 +0530

    added function printList constructLinkedList

--- src/ahesh/problems/ReverseListRecursive.java
+++ src/ahesh/problems/ReverseListRecursive.java
@@ -1,13 +1,35 @@
 package ahesh.problems;
 
-public class ReverseListRecursive {
-	class ListNode{
-		ListNode next;
-		int val;
-		
-		ListNode(){}
-		ListNode(int val) { this.val = val; }
-		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-	}
 
+
+//class ListNode{
+//	ListNode next;
+//	int val;
+//	
+//	ListNode(){}
+//	ListNode(int val) { this.val = val; }
+//	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//}
+
+public class ReverseListRecursive {
+	
+	public static ListNode constructLinkedList(int[] arr) {
+		ListNode head = new ListNode();
+		ListNode temp = head;
+		for(int i : arr) {
+			ListNode node = new ListNode(i);
+			temp.next = node;
+			temp = node;
+		}
+		
+		return head.next;
+	}
+	
+	public static void printList(ListNode head) {
+		while(head != null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+	
 }
