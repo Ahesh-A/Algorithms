c4f4873e4dfca690762fcccb18a92008b2dfe00e
Ahesh-A <aheshalagu@gmail.com>
2024-07-07 19:39:01 +0530

    added class ListNode

---
+++ src/ahesh/problems/ReverseListRecursive.java
@@ -0,0 +1,13 @@
+package ahesh.problems;
+
+public class ReverseListRecursive {
+	class ListNode{
+		ListNode next;
+		int val;
+		
+		ListNode(){}
+		ListNode(int val) { this.val = val; }
+		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+
+}
