61a32c12a5d0ff9efa1d3c1b1981fb33d6b659df
Ahesh-A <aheshalagu@gmail.com>
2024-07-07 20:52:28 +0530

    added function removeNode

--- src/ahesh/problems/RemoveElement.java
+++ src/ahesh/problems/RemoveElement.java
@@ -19,4 +19,31 @@ public class RemoveElement {
 			head = head.next;
 		}
 	}
+	
+	public static ListNode removeNode(ListNode head, int val) {
+		ListNode res = new ListNode();
+		ListNode prev = res;
+		res.next = head;
+		ListNode temp = head;
+			
+		while(temp != null) {
+			
+			if(temp.val == val) {
+				temp = temp.next; 
+				prev.next.next = null;
+				prev.next = temp;
+			} else {
+				prev = temp;
+				temp = temp.next;
+			}
+		}
+		
+		return res.next;
+	}
+	
+	public static void main(String[] args) {
+		ListNode head = constructLinkedList(new int[] {4, 4, 4, 1, 2, 3, 4, 4, 4, 5, 6, 4, 4});
+		ListNode res = removeNode(head, 4);
+		printList(res);
+	}
 }
