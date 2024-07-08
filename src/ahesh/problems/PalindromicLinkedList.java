82cd429745079ce84f96572004797da78248a122
ahesh-a <aheshalagu@gmail.com>
2024-07-08 20:54:56 +0530

    added function constructLinkedList and printList

--- src/ahesh/problems/PalindromicLinkedList.java
+++ src/ahesh/problems/PalindromicLinkedList.java
@@ -1,5 +1,24 @@
 package ahesh.problems;
 
 public class PalindromicLinkedList {
- 
+	public static ListNode constructLinkedList(int[] arr) {
+		ListNode head = new ListNode();
+		ListNode temp = head;
+		for(int i : arr) {
+			ListNode node = new ListNode(i);
+			temp.next = node;
+			temp = node;
+		}
+		
+		return head.next;
+	}
+	
+	public static void printList(ListNode head) {
+		while(head != null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+	
+	
 }
