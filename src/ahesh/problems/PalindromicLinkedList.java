81b8a49d011f88b73265a597a794f38932cf147c
ahesh-a <aheshalagu@gmail.com>
2024-07-08 21:24:00 +0530

    added function reverseList

--- src/ahesh/problems/PalindromicLinkedList.java
+++ src/ahesh/problems/PalindromicLinkedList.java
@@ -1,5 +1,7 @@
 package ahesh.problems;
 
+import java.util.List;
+
 public class PalindromicLinkedList {
 	public static ListNode constructLinkedList(int[] arr) {
 		ListNode head = new ListNode();
@@ -20,5 +22,51 @@ public class PalindromicLinkedList {
 		}
 	}
 	
+	public static ListNode findMid(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		while (fast != null && fast.next != null ) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		
+		return slow;
+	}
+
+//	public static ListNode reverseList(ListNode head) {
+//		
+//		if(head.next == null) {
+//			return head;
+//		}
+//		
+//		ListNode res = reverseList(head.next);
+//		head.next.next = head;
+//		head.next = null;
+//		
+//		return res;
+//	}
+//	public static boolean isSubList(ListNode l1, ListNode l2) {
+//		
+//		while(l1 != null && l2 != null) {
+//			
+//			if(l1.val != l2.val) {
+//				return false;
+//			}
+//			
+//			l1 = l1.next;
+//			l2 = l2.next;
+//		}
+//		
+//		return true;
+//	}
+	
+	public static void main(String[] args) {
+		ListNode head = constructLinkedList(new int[] {1, 2, 2, 1});
+		ListNode mid = findMid(head);
+		ListNode res = reverseList(mid);
+		printList(res);
+		System.out.println(isSubList(res, head));
+	}
 	
 }
