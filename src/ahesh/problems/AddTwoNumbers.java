89cbdfb0e5b726b1f617a6236e46960492a48ceb
ahesh-a <aheshalagu@gmail.com>
2024-07-10 07:49:13 +0530

    added function constructLinkedList and printList

--- src/ahesh/problems/AddTwoNumbers.java
+++ src/ahesh/problems/AddTwoNumbers.java
@@ -21,6 +21,25 @@
 package ahesh.problems;
 
 public class AddTwoNumbers {
+	public static ListNode constructLinkedList(int[] arr) {
+		ListNode head = new ListNode();
+		ListNode temp = head;
+		for(int i : arr) {
+			ListNode node = new ListNode(i);
+			temp.next = node;
+			temp = node;
+		}
+		
+		return head.next;
+	}
+	
+	public static void printList(ListNode head) {
+		while(head != null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 	}
