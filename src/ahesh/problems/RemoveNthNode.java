52f9e142289acc0c4efb565e02cdc48ec6b67fdc
ahesh-a <aheshalagu@gmail.com>
2024-07-10 22:50:00 +0530

    added function constructListNode and printList

--- src/ahesh/problems/RemoveNthNode.java
+++ src/ahesh/problems/RemoveNthNode.java
@@ -1,5 +1,27 @@
 package ahesh.problems;
 
 public class RemoveNthNode {
-
+	public static ListNode constructLinkedList(int[] arr) {
+		ListNode head = new ListNode();
+		ListNode temp = head;
+		for(int i : arr) {
+			ListNode node = new ListNode(i);
+			temp.next = node;
+			temp = node;
+		}
+		
+		return head.next;
+	}
+	
+	public static void printList(ListNode head) {
+		while(head != null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		ListNode l = constructLinkedList(new int[] {1, 2, 3, 4, 5, 6, 7, 8});
+		
+	}
 }
