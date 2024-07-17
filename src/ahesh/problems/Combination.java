04d8c70260be88b3309a9b16f9a42dcaa2adac7e
Ahesh-A <aheshalagu@gmail.com>
2024-07-17 22:29:34 +0530

    added function printComb

--- src/ahesh/problems/Combination.java
+++ src/ahesh/problems/Combination.java
@@ -2,4 +2,17 @@ package ahesh.problems;
 
 public class Combination {
 	
+	public static void printComb(int[] arr, int idx, String str) {
+		if(idx == arr.length) {
+			System.out.println(str);
+		}
+		
+		for(int i = idx; i < arr.length; i++) {
+			printComb(arr, i + 1, str + arr[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5};
+	}
 }
