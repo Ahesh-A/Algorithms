feb3ab033c74996c7cac59ce31be7ac462f08a39
Ahesh-A <aheshalagu@gmail.com>
2024-07-17 22:31:16 +0530

    added function swapHelper

--- src/ahesh/problems/SwapNodes.java
+++ src/ahesh/problems/SwapNodes.java
@@ -4,15 +4,41 @@ import org.ahesh.types.ListNode;
 
 public class SwapNodes {
 	
+	public static void swapHelper(ListNode parent, ListNode p1, ListNode p2) {
+		p1.next = p2.next;
+		p2.next = p1;
+		parent.next = p2;
+	}
+	
 	public static ListNode swapNodes(ListNode head) {
+		
+		if(head == null) {
+			return head;
+		}
+		
 		ListNode node = new ListNode();
 		node.next = head;
 		
+		ListNode parent = node;
+		ListNode pointer1 = head;
+		ListNode pointer2 = head.next;
+		
+		while(pointer1 != null && pointer2 != null) {
+			swapHelper(parent, pointer1, pointer2);
+			if(parent.next == null || pointer1.next == null) {
+				return node.next;
+			}
+			
+			parent = pointer1;				
+			pointer1 = parent.next;				
+			pointer2 = pointer1.next;
+		}
 		
 		return node.next;
 	}
 	public static void main(String[] args) {
-		ListNode head = ListNode.constructLinkedList(new int[] {1, 2, 3, 4, 5, 6, 7, 8});
+		ListNode head = ListNode.constructLinkedList(new int[] {1, 7});
+		head = swapNodes(head);
 		ListNode.printList(head);
 	}
 }
