123db143b4f5c94a949784421a352bb3ac02061b
ahesh-a <aheshalagu@gmail.com>
2024-07-18 22:30:23 +0530

    added function getNthNodeFromLast

--- src/ahesh/problems/ReverseKGroup.java
+++ src/ahesh/problems/ReverseKGroup.java
@@ -1,5 +1,33 @@
 package ahesh.problems;
 
+import org.ahesh.types.ListNode;
 public class ReverseKGroup {
+	public static ListNode getNthNodeFromLast(ListNode head, int n) {
+			
+		ListNode sudoHead = new ListNode();
+		sudoHead.next = head;
+		ListNode temp = sudoHead;
+		ListNode pointer = sudoHead;
+		
+		for (int i = 0; i < n; i++) {
+			if(temp == null) {
+				return null;
+			}
+			
+			temp = temp.next;
+		}
+		
+		while(temp != null) {
+			temp = temp.next;
+			pointer = pointer.next;
+		}
+		
+		return pointer;
+	}
 	
+	public static void main(String[] args) {
+		ListNode head = ListNode.constructLinkedList(new int [] {1, 2, 3, 4, 5, 6, 7, 8});
+		ListNode pointer = getNthNodeFromLast(head, 2);
+		System.out.println(pointer.val);
+	}
 }
