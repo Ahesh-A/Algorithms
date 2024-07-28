52f1a5f9db826d1de898e3439b6a9e8666f93556
ahesh-a <aheshalagu@gmail.com>
2024-07-28 19:47:42 +0530

    added class ReverseKByBroup

---
+++ src/ahesh/problems/ReverseKByGroup.java
@@ -0,0 +1,12 @@
+package ahesh.problems;
+
+import org.ahesh.types.ListNode;
+
+public class ReverseKByGroup {
+	
+	
+	public static void main(String[] args) {
+		ListNode head = ListNode.constructLinkedList(new int[] {1, 2, 3, 4, 5, 6, 7 });
+		ListNode.printList(head);
+	}
+}
