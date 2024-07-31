106248aa5d34f9b97356bccff15df7e549d4a50b
ahesh-a <aheshalagu@gmail.com>
2024-07-31 22:54:05 +0530

    added function getDuplicate

--- src/ahesh/problems/FindDuplicate.java
+++ src/ahesh/problems/FindDuplicate.java
@@ -1,5 +1,24 @@
 package ahesh.problems;
 
-public class FindDuplicate {
+import java.util.HashSet;
+import java.util.Set;
 
+public class FindDuplicate {
+	
+	public static int getDuplicate(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+		for(int i : arr) {
+			if(set.contains(i)) {
+				return i;
+			} else {
+				set.add(i);
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 3, 2, 5, 4, 6, 6, 7};
+		System.out.println(getDuplicate(arr));
+	}
 }
