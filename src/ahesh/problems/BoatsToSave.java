6d88e250208fa150eaf1f69a1efa7f7ae8954acb
ahesh-a <aheshalagu@gmail.com>
2024-08-06 23:12:37 +0530

    added function getBoats

--- src/ahesh/problems/BoatsToSave.java
+++ src/ahesh/problems/BoatsToSave.java
@@ -1,5 +1,33 @@
 package ahesh.problems;
 
-public class BoatsToSave {
+import java.util.Arrays;
 
+public class BoatsToSave {
+	
+	public static int getBoats(int[] arr, int limit) {
+		int i = 0;
+		int j = arr.length - 1;
+		int count = 0;
+		
+		while(i <= j) {
+			int sum = arr[i] + arr[j];
+			
+			if(sum <= limit) {
+				i++;
+				j--;
+			} else {
+				j--;
+			}
+			
+			count++;
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 1, 1};
+		Arrays.sort(arr);
+		System.out.println("Res: " + getBoats(arr, 2));
+	}
 }
