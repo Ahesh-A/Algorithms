6674c829cc95fbc379cf26ac2b7975f12f9200cc
ahesh-a <aheshalagu@gmail.com>
2024-08-08 22:36:34 +0530

    added function main

--- src/ahesh/problems/SumCondition.java
+++ src/ahesh/problems/SumCondition.java
@@ -1,5 +1,9 @@
 package ahesh.problems;
 
 public class SumCondition {
-
+	public static void main (String args[]) {
+		int[] arr = {3, 5, 7, 2};
+		int target = 9;
+		System.out.println("Result: " + getNoOfSequenceAddToTarget(arr, target));
+	}
 }
