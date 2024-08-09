91845d43b4c89d7ea15e7c95945a68f861a761a2
ahesh-a <aheshalagu@gmail.com>
2024-08-09 21:10:23 +0530

    added function getBST

--- src/ahesh/problems/SumCondition.java
+++ src/ahesh/problems/SumCondition.java
@@ -1,6 +1,34 @@
 package ahesh.problems;
 
+import java.util.Arrays;
+
 public class SumCondition {
+	public static int getNoOfSequenceAddToTarget(int[] arr, int target) {
+		int[] exp = new int[arr.length];
+		exp[0] = 1;
+		int res = 0;
+		
+		for(int i = 1; i < arr.length; i++) {
+			exp[i] = (exp[i - 1] * 2) % 1000000007;
+		}
+		
+		Arrays.sort(arr);
+		
+		int i = 0; 
+		int j = arr.length - 1;
+		
+		while(i <= j) {
+			if(arr[i] + arr[j] <= target) {
+				res = (res + exp[j- i]) % 1000000007;
+				i++;
+			} else {
+				j--;
+			}
+		}
+		
+		return res;
+	}
+	
 	public static void main (String args[]) {
 		int[] arr = {3, 5, 7, 2};
 		int target = 9;
--- src/ahesh/problems/TwoPointerInBST.java
+++ src/ahesh/problems/TwoPointerInBST.java
@@ -1,5 +1,16 @@
 package ahesh.problems;
 
+import org.ahesh.types.TreeNode;
+
+import myds.binarysearchtree.*;
+
 public class TwoPointerInBST {
-	
+	public static TreeNode<Integer> getBST(Integer[] arr) throws Exception {
+		TreeNode<Integer> root = BSTUtils.constructBST(arr);
+		
+		return root;
+	}
+	public static void main(String[] args) throws Exception {
+		
+	}
 }
