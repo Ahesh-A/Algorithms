c103c774fef1f85e940c9a66ad0d6a8f875f1070
ahesh-a <aheshalagu@gmail.com>
2024-08-10 11:51:11 +0530

    added function doesPartitionExist

--- src/ahesh/problems/PartitionSum.java
+++ src/ahesh/problems/PartitionSum.java
@@ -1,9 +1,21 @@
 package ahesh.problems;
 
+import java.util.Arrays;
+
 public class PartitionSum {
+	
+	public static boolean doesPartitionExist(int[] arr) {
+		int sum = Arrays.stream(arr).sum();
+		
+		if(sum % 2 != 0) {
+			return false;
+		}
+	}
 	
 	public static void main(String[] args) {
 		int[] arr = {1, 1, 1, 3};
+		
+		
 		
 	}
 }
