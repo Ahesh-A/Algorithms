437afeab02ebc9694ebd1c9bf236ff19db81ed11
ahesh-a <aheshalagu@gmail.com>
2024-08-10 12:41:24 +0530

    added code to stop at first occurance

--- src/ahesh/problems/PartitionSum.java
+++ src/ahesh/problems/PartitionSum.java
@@ -6,7 +6,21 @@ import java.util.List;
 
 public class PartitionSum {
 	
-	
+	public static void printPartition(int[] arr, List<Integer> list) {
+		List<Integer> basket1 = new ArrayList<>();
+		List<Integer> basket2 = new ArrayList<>();
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(list.contains(i)) {
+				basket1.add(arr[i]);
+			} else {
+				basket2.add(arr[i]);
+			}
+		}
+		
+		System.out.println(basket1 + " | "  + basket2);
+		
+	}
 	
 	public static boolean partitionExistOdd(int[] arr, int target, int sum, int idx, List<Integer> list) {
 		if(idx == arr.length) {
@@ -14,16 +28,21 @@ public class PartitionSum {
 		}
 		
 		if (sum == target) {
-			System.out.println(list);
+			printPartition(arr, list);
 			return true;
 		}
 		
 		list.add(idx);
 		boolean include =  partitionExistOdd(arr, target, sum + arr[idx], idx + 1, list);
 		list.remove(list.size() - 1);
+		
+		if(include) {
+			return true;
+		}
+		
 		boolean skip = partitionExistOdd(arr, target, sum, idx + 1, list);
 		
-		return include | skip;
+		return skip;
 	}
 	
 	public static boolean partitionExistEven(int[] arr, int target, int sum, int idx, int count, List<Integer> list) {
@@ -32,16 +51,21 @@ public class PartitionSum {
 		}
 		
 		if(sum == target && count != arr.length / 2) {
-			System.out.println(list);
+			printPartition(arr, list);
 			return true;
 		}
 		
 		list.add(idx);
 		boolean include = partitionExistEven(arr, target, sum + arr[idx], idx + 1, count + 1, list);
 		list.remove(list.size() - 1);
+		
+		if(include) {
+			return true;
+		}
+		
 		boolean skip = partitionExistEven(arr, target, sum, idx + 1, count, list);
 		
-		return include | skip;
+		return skip;
 	}
 	
 	public static boolean doesPartitionExist(int[] arr) {
@@ -62,7 +86,7 @@ public class PartitionSum {
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = {1, 1, 1, 1, 4};
+		int[] arr = {1, 2, 3, 3};
 		
 		System.out.println("Result: " + doesPartitionExist(arr));
 		
