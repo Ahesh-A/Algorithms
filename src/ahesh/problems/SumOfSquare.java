005f60e18070759fc32f4c06d34a709de1e7bc1d
ahesh-a <aheshalagu@gmail.com>
2024-08-11 10:59:48 +0530

    solved problem

--- src/ahesh/problems/SumOfSquare.java
+++ src/ahesh/problems/SumOfSquare.java
@@ -15,7 +15,48 @@
 package ahesh.problems;
 
 public class SumOfSquare {
-	public static void main(String[] args) {
+	public static int getRight(int target) {
+		long left = 0;
+		long right = target;
+		long mid = -1;
 		
+		while (left <= right) {
+			mid = left + (right - left) / 2;
+			long val = mid * mid;
+			
+			if(val == target) {
+				return (int)mid;
+			} else if(val < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		
+		return (int)right;
+	}
+	
+	public static boolean isIntegerSumExist(int target) {
+		long right = (int)Math.sqrt(target);
+		long left = 0;
+//		System.out.println(target+ " " + getRight(target));
+		while(left <= right) {
+			long num = left * left + right * right;
+//			System.out.println(num == target);
+			System.out.println(num + " " + target);
+//			break;
+			if( num == target) {
+				return true;
+			} else if(num < target) {
+				left ++;
+			} else {
+				right --;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Result: " + isIntegerSumExist(2147483600));
 	}
 }
