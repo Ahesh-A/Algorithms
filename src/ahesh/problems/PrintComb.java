a516a46e1177a07ebf53a7b03913b279acbd0fd2
ahesh-a <aheshalagu@gmail.com>
2024-08-20 21:19:09 +0530

    modified PrintComb

--- src/ahesh/problems/PrintComb.java
+++ src/ahesh/problems/PrintComb.java
@@ -5,21 +5,29 @@ import java.util.List;
 
 public class PrintComb {
 	
-	public static void printComb(List<Integer> list, Integer[] arr, int idx) {
+	public static void printComb(List<Integer> list, Integer[] arr, int idx, List<List<Integer>> ans, int k) {
+		
 		if(idx == arr.length) {
-			System.out.println(list);
+			if(list.size() == k) {				
+				ans.add(new ArrayList<>(list));
+//				System.out.println(list);
+			}
 			return;
 		}
 		
 		list.add(arr[idx]);
-		printComb(list, arr, idx + 1);
+		printComb(list, arr, idx + 1, ans, k);
 		list.remove(list.size() - 1);
-		printComb(list, arr, idx + 1);
+		printComb(list, arr, idx + 1, ans, k);
 	}
 	
 	public static void main(String[] args) throws Exception {
 		Integer [] arr = {1, 3 , 4, 2};
 		List<Integer> list = new ArrayList<Integer>();
-		printComb(list, arr, 0);
+		List<List<Integer>> ans = new ArrayList<>();
+		
+		printComb(list, arr, 0, ans, );
+		
+		System.out.println(ans);
 	}
 }
