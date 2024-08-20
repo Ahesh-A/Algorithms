477c366c78be9d85a829d4f95da613e79aeeb603
ahesh-a <aheshalagu@gmail.com>
2024-08-20 22:48:29 +0530

    added function maxArea

--- src/ahesh/problems/ContainerWater.java
+++ src/ahesh/problems/ContainerWater.java
@@ -27,7 +27,29 @@
 package ahesh.problems;
 
 public class ContainerWater {
+	public static int maxArea(int[] height) {
+		int res = 0;
+		int i = 0;
+		int j = height.length - 1;
+		
+		while(i < j) {
+			int area = (j - i) * (Integer.min(height[i], height[j]));
+			if(res < area) {
+				res = area;
+			}
+			
+			if(height[i + 1] < height[j - 1]) {
+				j--;
+			}else {
+				i++;
+			}
+		}
+		
+		return res;
+	}
+	
 	public static void main(String[] args) {
-		int[] arr = {1,8,6,2,5,4,8,3,7}; 
+		int[] arr = {1,8,6,2,5,4,8,3,7};
+		System.out.println("Result: " + maxArea(arr));
 	}
 }
