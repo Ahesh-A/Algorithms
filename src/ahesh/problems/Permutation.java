1f2e3f7e742609fc3590d0647e986fce637ea59b
ahesh-a <aheshalagu@gmail.com>
2024-08-25 10:25:50 +0530

    added function to get permutation

--- src/ahesh/problems/Permutation.java
+++ src/ahesh/problems/Permutation.java
@@ -1,8 +1,37 @@
 package ahesh.problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Permutation {
-		public static void main(String[] args) {
-			int[] arr = {1, 2, 3};
+		public static void swap(int i, int j, Integer[] arr) {
+			if (i == j) return;
 			
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		
+		public static void getPermutationList(int idx, Integer[] arr, List<List<Integer>> res){
+			if(idx == arr.length) {
+				System.out.print(Arrays.toString(arr));
+				res.add(new ArrayList<>(Arrays.asList(arr)));
+				return;
+			}
+			
+			for(int i = idx; i < arr.length; i++) {
+				swap(idx, i, arr);
+				getPermutationList(idx + 1, arr, res);
+				swap(idx, i, arr);
+			}
+			
+		}
+		
+		public static void main(String[] args) {
+			Integer[] arr = {1, 2, 3};
+			List<List<Integer>> list = new ArrayList<>();
+			getPermutationList(0, arr, list);
+			System.out.println("Result: " + list);
 		}
 }
