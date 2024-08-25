516a0e054a3d22516d8144f39b9d5508a6ed426a
ahesh-a <aheshalagu@gmail.com>
2024-08-25 22:52:26 +0530

    added function swap

--- src/ahesh/problems/NextPermutation.java
+++ src/ahesh/problems/NextPermutation.java
@@ -50,11 +50,11 @@ public class NextPermutation {
 //		}
 //	}
 	
-//	public static void swap(int i, int j, int[] arr){
-//		int temp = arr[i];
-//		arr[i] = arr[j];
-//		arr[j] = temp;
-//	}
+	public static void swap(int i, int j, int[] arr){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 	
 	public static int getIndexForNumberLessThanUpcommingNumber(int[] arr) {
 		int len = arr.length;
